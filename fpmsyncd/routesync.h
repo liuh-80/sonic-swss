f4b583a0731de503528ad07ce53b577d3f980559
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-07-11 02:02:52 +0800

    Fix fpmsyncd crash during pfcwd/test_pfcwd_warm_reboot.py worm reboot issue (#3746)
    Fix fpmsyncd crash during warm reboot issue
    
    Why I did it
    The SONiC management test case pfcwd/test_pfcwd_warm_reboot.py failed due to a crash in fpmsyncd during the warm reboot process.
    
    The crash occurred when fpmsyncd received the warm reboot signal and invoked the WarmStartHelper::checkAndStart() method. At that moment, the m_syncTable inside WarmStartHelper pointed to an invalid route table, which triggered a segmentation fault (SIGSEGV).
    
    Root Cause:
    In the constructor of RouteSync, the initialization of m_warmStartHelper depends on m_routeTable being initialized first. However, in the class declaration, m_warmStartHelper is listed before m_routeTable.
    
    In C++, member variables are initialized in the order they are declared in the class—not in the order specified in the constructor’s initializer list. As a result, m_warmStartHelper is initialized before m_routeTable, leading to m_warmStartHelper referencing an uninitialized route table.

--- fpmsyncd/routesync.h
+++ fpmsyncd/routesync.h
@@ -66,7 +66,10 @@ public:
         m_fpmInterface = nullptr;
     }
 
-    WarmStartHelper  m_warmStartHelper;
+    WarmStartHelper& getWarmStartHelper()
+    {
+        return m_warmStartHelper;
+    }
 
 private:
     /* ZMQ client */
@@ -79,6 +82,8 @@ private:
     ProducerStateTable  m_vnet_routeTable;
     /* vnet vxlan tunnel table */  
     ProducerStateTable  m_vnet_tunnelTable;
+    /* Warm start helper */
+    WarmStartHelper m_warmStartHelper;
     /* srv6 mySid table */
     ProducerStateTable m_srv6MySidTable; 
     /* srv6 sid list table */
